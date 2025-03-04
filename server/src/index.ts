3e998474a2e693157e0c3910f48afa49a73f6390
angel9154 <angelgerenavazquez@outlook.com>
2025-03-04 08:31:06 -0500

    added the sign in authentication using aws cognito

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 /* server */
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
