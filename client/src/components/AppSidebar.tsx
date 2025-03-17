c162b82897139c32fbaace787c1c034be6b5beff
angel9154 <angelgerenavazquez@outlook.com>
2025-03-17 16:15:35 -0400

    add dashboard layout and sidebar components; adjust navbar height

---
+++ client/src/components/AppSidebar.tsx
@@ -0,0 +1,82 @@
+import { Building, FileText, Heart, Home, Menu, Settings, X } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { Sidebar, SidebarHeader, SidebarMenu, SidebarMenuItem } from './ui/sidebar'
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { cn } from '@/lib/utils'
+
+const AppSidebar = ({ userType }: AppSidebarProps) => {
+    const pathname = usePathname()
+    const { toggleSidebar, open } = useSidebar()
+
+const navLinks = 
+userType === "manager"
+? [
+    { icon: Building, label: "Properties", href: "/managers/properties"},
+    { icon: FileText, label: "Applications", href: "/managers/applications"},
+    { icon: Settings, label: "Settings", href: "/managers/settings"},
+    
+
+
+]
+:[
+    { icon: Heart, label: "Favorites", href: "/tentants/favorites"},
+    { icon: FileText, label: "Applications", href: "/tentants/applications"},
+    { icon: Home, label: "Residences", href: "/tentants/residences"},
+
+
+    { icon: Settings, label: "Settings", href: "/tentants/settings"},
+
+]
+
+  return (
+    <Sidebar
+    collapsible='icon'
+    className="fixed left-0 bg-white shadow-lg"
+    style={{
+        top: `${NAVBAR_HEIGHT}px`,
+        height: `calc(100vh - ${NAVBAR_HEIGHT})`
+    }}
+    >
+    <SidebarHeader>
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <div
+                className={cn(
+                    "flex min-h-[56px] w-full items-center pt-3 mb-3", 
+                    open ? "justify-between px-6" : "justify-center"
+                )}
+                >
+                    {open ? (
+                        <>
+                         <h1 className='text-xl font-bold text-gray-800'>
+                            {userType === "manager" ? "Manager View" : "Renter View"}
+                         </h1>
+                         <button
+                         className='hover:bg-gray-100 p-2 rounded-md'
+                         onClick={() => toggleSidebar()}
+                         >
+                            <X className='h-6 w-6 text-grey-600' />
+  
+                         </button>
+                        </>
+                    ) : (
+                        <button
+                        className='hover:bg-gray-100 p-2 rounded-md'
+                        onClick={() => toggleSidebar()}
+                        >
+                           <Menu className='h-6 w-6 text-grey-600' />
+ 
+                        </button>
+                    )
+                    
+                }
+                </div>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    </SidebarHeader>
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
